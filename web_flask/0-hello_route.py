a4306fb6d8386cb7ba3f2b51fcdef17709a8672a
KID-CAPRI <nardoskassa@Nardoss-MacBook-Air.local>
2023-01-13 18:18:52 +0300

    Task 0: A script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Starts Flask web-app
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+of __name__=="__main__":
+    app.run(host="0.0.0.0")
