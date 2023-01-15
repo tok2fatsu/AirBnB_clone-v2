f24cfa8d68b3c2aa66d12ba50bfe00ca0c15d23a
tok2fatsu <mailit2moi@gmail.com>
2023-01-15 13:46:04 -0800

    Task 8: List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
 """Starts a Flask web app
 """
+from flask import Flask, render_template
+
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
+    """Lists states
+    Returns:
+        string: simple message
     """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+    return render_template("7-states_list.html", states=list(states))
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(self):
+    """ Cleanup Db Session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    storage.reload()
+    app.run()
